3e0b0faf214b9616106ee16311effd680a7e2038
netiriska <i.sidik@ukr.net>
2023-07-16 19:43:55 +0300

    header styles

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,18 @@
 import { Link, Outlet } from "react-router-dom";
-import logo from "../../images/header/bookmark-heart-fill.svg";
+import logo from "../../images/header/bookmark-heart.svg";
+import "./styles.css";
 
 export default function Header() {
   return (
     <header>
-      <a href="/" className="main-header">
-        WITH LOVE 
-        <img src={ logo } alt="" /> BOOK STORE / Khomenko Iryna
-      </a>
+      <div className="container">
+        <div className="authorization">
+          <a href="/" className="main-header">
+            WITH LOVE
+            <img src={logo} alt="" /> BOOK STORE / Khomenko Iryna
+          </a>
+        </div>
+      </div>
       <nav>
         <div>
           <Link to="/">Signin</Link>
