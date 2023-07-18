849b774594f6b4faff58d5b16e7281f839160516
netiriska <i.sidik@ukr.net>
2023-07-18 09:26:51 +0300

    rule for input with message

--- src/components/signin/Signin.jsx
+++ src/components/signin/Signin.jsx
@@ -1,8 +1,30 @@
+import React, { useState, useEffect } from "react";
 import avatar from "../../images/signin/avatar.png";
 import "./styles.css";
 import Footer from "../footer/footer";
 
 export default function Signin() {
+  const [disabled, setDisabled] = useState(true);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+  const [isUserTyping, setIsUserTyping] = useState(false);
+
+  const handleChange = (e) => {
+    setIsUserTyping(true);
+    if (e.target.value.length >= 4 && e.target.value.length <= 16) {
+      setDisabled(false);
+      setShowErrorMessage(false);
+    } else {
+      setDisabled(true);
+      setShowErrorMessage(true);
+    }
+  };
+
+  useEffect(() => {
+    if (disabled) {
+      setShowErrorMessage(true);
+    }
+  }, [disabled]);
+
   return (
     <div>
       <h1>Signin page</h1>
@@ -21,15 +43,27 @@ export default function Signin() {
                 id="username"
                 name="user_name"
                 placeholder="Username"
+                onChange={handleChange}
               />
+              {showErrorMessage && isUserTyping && (
+                <span className="input_error-message">
+                  Username must have at least 4 characters but cannot exceed 16
+                  characters
+                </span>
+              )}
             </div>
-            <button className="sign-in__button" type="submit" name="submit">
+            <button
+              className="sign-in__button"
+              type="submit"
+              name="submit"
+              disabled={disabled}
+            >
               Sign-In
             </button>
           </form>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
