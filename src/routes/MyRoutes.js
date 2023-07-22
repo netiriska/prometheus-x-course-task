e1654a40a908e156096be5b86326828f64d94373
netiriska <i.sidik@urk.net>
2023-07-22 15:41:03 +0300

    context header add username

--- src/routes/MyRoutes.js
+++ src/routes/MyRoutes.js
@@ -1,20 +1,25 @@
 import { Routes, Route } from "react-router-dom";
-
 import Signin from "../components/signin/Signin";
 import BookList from "../components/book-list/BookList";
 import SpecificBook from "../components/specific-book/SpecificBook";
 import Header from "../components/header/header";
 import PageNotFound from "../components/page-not-found/PageNotFound";
+import { useState } from "react";
+import UserContext from "../context/UserContext";
 
 export default function MyRoutes() {
+  const [user, setUser] = useState(null);
+
   return (
-    <Routes>
-      <Route path="/" element={<Header />}>
-        <Route path="/" element={<Signin />} />
-        <Route path="/specificbook" element={<SpecificBook />} />
-        <Route path="/booklist" element={<BookList />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Route>
-    </Routes>
+    <UserContext.Provider value={user}>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route path="/" element={<Signin setUser={setUser} />} />
+          <Route path="/specificbook" element={<SpecificBook />} />
+          <Route path="/booklist" element={<BookList />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </UserContext.Provider>
   );
 }
