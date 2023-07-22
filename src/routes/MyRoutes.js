3ebde99b87c35e710bc4e1f2693a29599dc1512a
netiriska <i.sidik@ukr.net>
2023-07-22 19:05:48 +0300

    private path - return to signin if not login

--- src/routes/MyRoutes.js
+++ src/routes/MyRoutes.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Signin from "../components/signin/Signin";
 import BookList from "../components/book-list/BookList";
 import SpecificBook from "../components/specific-book/SpecificBook";
@@ -9,14 +9,36 @@ import UserContext from "../context/UserContext";
 
 export default function MyRoutes() {
   const [user, setUser] = useState(null);
+  // https://www.robinwieruch.de/react-router-private-routes/
+  const ProtectedRoute = ({ user, redirectPath = "/", children }) => {
+    if (!user) {
+      return <Navigate to={redirectPath} replace />;
+    }
+
+    return children;
+  };
 
   return (
     <UserContext.Provider value={user}>
       <Routes>
         <Route path="/" element={<Header setUser={setUser} />}>
           <Route path="/" element={<Signin setUser={setUser} />} />
-          <Route path="/specificbook" element={<SpecificBook />} />
-          <Route path="/booklist" element={<BookList />} />
+          <Route
+            path="/specificbook"
+            element={
+              <ProtectedRoute user={user}>
+                <SpecificBook />
+              </ProtectedRoute>
+            }
+          ></Route>
+          <Route
+            path="/booklist"
+            element={
+              <ProtectedRoute user={user}>
+                <BookList />
+              </ProtectedRoute>
+            }
+          ></Route>
           <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
