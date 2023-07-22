2938a2c95c422c6b7757b7f5dea5f92619ebe8cc
netiriska <i.sidik@urk.net>
2023-07-22 20:28:17 +0300

    booklist align books

--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -1,9 +1,17 @@
 export default function Book(props) {
   return (
-    <div>
-      <h1>
-        {props.title} {props.author}
-      </h1>
-    </div>
+    <article className="book">
+      <p className="book__author">
+        <strong>Book author: </strong>
+        {props.author}
+      </p>
+      <p className="book__price">{props.price}</p>
+      <img className="book__image" src={props.image} alt="book image" />
+      <p className="book__title">
+        <strong>Book name: </strong>
+        {props.title}
+      </p>
+      <button className="book__button">View</button>
+    </article>
   );
 }
