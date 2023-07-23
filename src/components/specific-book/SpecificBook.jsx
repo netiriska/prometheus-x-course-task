13457e07ab00494c3013bb1e79b50c6f6346212b
netiriska <i.sidik@ukr.net>
2023-07-23 11:17:26 +0300

    specific book className

--- src/components/specific-book/SpecificBook.jsx
+++ src/components/specific-book/SpecificBook.jsx
@@ -6,16 +6,16 @@ export default function SpecificBook(props) {
       <h1>SpecificBook page</h1>
       <div className="wrapper">
         <main>
-          <div class="specific-book">
-            <div class="container">
-              <div class="bookinfo">
-                <div class="specific-book__column specific-book__image">
+          <div className="specific-book">
+            <div className="container">
+              <div className="bookinfo">
+                <div className="specific-book__column specific-book__image">
                   <img
                     src={props.image}
                     alt="JavaScript: The Definitive Guide, 7th Edition book"
                   />
                 </div>
-                <div class="specific-book__column specific-book__info">
+                <div className="specific-book__column specific-book__info">
                   <p>
                     <strong>Book name: </strong>JavaScript: The Definitive
                     Guide, 7th Edition
@@ -30,26 +30,26 @@ export default function SpecificBook(props) {
                     <strong>Book tags: </strong>core
                   </p>
                 </div>
-                <div class="specific-book__column specific-book__calculation">
-                  <div class="price">
+                <div className="specific-book__column specific-book__calculation">
+                  <div className="price">
                     <p>Price</p>
                     <p id="pricePerBook">$52.72</p>
                   </div>
                   <form action="/">
-                    <div class="choice">
+                    <div className="choice">
                       <label for="quantity">Count</label>
                       <input type="number" id="quantity" min="1" max="42" />
                     </div>
                   </form>
-                  <div class="totalPrice">
+                  <div className="totalPrice">
                     <div>Total price</div>
                     <p id="totalPrice">0.00</p>
                   </div>
                   <button type="submit">Add to cart</button>
                 </div>
               </div>
-              <div class="bookview">
-                <div class="specific-book__column specific-book__description">
+              <div className="bookview">
+                <div className="specific-book__column specific-book__description">
                   <p>
                     <strong>Description: </strong>JavaScript is the programming
                     language of the web and is used by more software developers
