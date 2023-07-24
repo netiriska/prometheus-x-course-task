80805131f540d96fcf17f70a1ad215d5eabb0d60
netiriska <i.sidik@urk.net>
2023-07-24 20:57:29 +0300

    link to cart bu image in header

--- src/components/header/headerActions.jsx
+++ src/components/header/headerActions.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import UserContext from "../../context/UserContext";
 import cart from "../../images/header/cart.svg";
 import avatar from "../../images/signin/icon-username.png";
@@ -10,11 +11,17 @@ export default function HeaderActions(props) {
   const user = useContext(UserContext);
   console.log(user);
 
+  const navigate = useNavigate();
+
+  const navigateCart = () => {
+    navigate("/cart");
+  };
+
   return (
     <div className="headerActions__container">
-      <a className="headerActions__cart" href="">
+      <button className="headerActions__cart" href="" onClick={navigateCart}>
         <img src={cart} alt="cart" />
-      </a>
+      </button>
       <button className="headerActions__button" onClick={handleLogout}>
         Sign-Out
       </button>
