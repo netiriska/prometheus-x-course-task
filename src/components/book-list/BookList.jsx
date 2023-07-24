a4106df79d2cb80a6d664abbcab4b7c5a446df32
netiriska <i.sidik@ukr.net>
2023-07-24 18:12:30 +0300

    search select style

--- src/components/book-list/BookList.jsx
+++ src/components/book-list/BookList.jsx
@@ -10,26 +10,32 @@ export default function BookList(props) {
 
   return (
     <div className="books__container">
-      <div className="search__container">
-        <form className="search__form">
-          <input
-            className="search__input"
-            type="search"
-            placeholder="Search by book name"
-          />
-          <button className="search__button" type="submit">
-            Search
-          </button>
-        </form>
-        <select className="select__container" name="Price">
-          <option value="" disabled hidden>
-            Price
-          </option>
-          <option value="all">All</option>
-          <option value="Up to 15">Up to 15</option>
-          <option value="From 15 to 30">From 15 to 30</option>
-          <option value="More than 30">More than 30</option>
-        </select>
+      <div className="search_select__container">
+        <div className="search__container">
+          <form className="search__form">
+            <input
+              className="search__input"
+              type="search"
+              placeholder="Search by book name"
+            />
+            <button className="search__button" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+        <div className="select__container">
+          <select name="Price">
+            <option value="" disabled hidden>
+              Price
+            </option>
+            <option className="select__all" value="all">
+              All
+            </option>
+            <option value="Up to 15">Up to 15</option>
+            <option value="From 15 to 30">From 15 to 30</option>
+            <option value="More than 30">More than 30</option>
+          </select>
+        </div>
       </div>
       <div className="books__cards">
         {books.books.map((book) => {
