0707cace689909252e28824112be465b14b1184d
netiriska <i.sidik@ukr.net>
2023-07-25 12:27:19 +0300

    cart style

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -7,8 +7,14 @@ export default function Cart() {
   const selectedBooks = useContext(SelectedBooksContext);
   console.log(selectedBooks);
   return (
-    <div className="cart__empty">
-      <img className="cart__image" src={cart} alt="cart" />;<p>Cart empty...</p>
+    <div className="cart__empty_purchase">
+      <div className="cart__purchase_button_container">
+        <button className="cart__purchase_button">Purchase</button>
+      </div>
+      <div className="cart__empty">
+        <img className="cart__image" src={cart} alt="cart" />
+        <p>Cart empty...</p>
+      </div>
     </div>
   );
 }
