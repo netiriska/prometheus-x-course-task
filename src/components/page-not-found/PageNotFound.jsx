73474593099c25b5f060bc8d7ae55a381be4875a
netiriska <i.sidik@ukr.net>
2023-07-30 14:56:20 +0300

    pageNotFound footer

--- src/components/page-not-found/PageNotFound.jsx
+++ src/components/page-not-found/PageNotFound.jsx
@@ -1,6 +1,8 @@
+import "./styles.css";
+
 export default function PageNotFound() {
   return (
-    <div>
+    <div className="pageNotFound">
       <p>Oops, something went wrong. 404 error</p>
     </div>
   );
