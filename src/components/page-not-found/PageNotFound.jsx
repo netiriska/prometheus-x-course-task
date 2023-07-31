046458e3976d2cce4dc221d81486447859cc5652
netiriska <i.sidik@urk.net>
2023-07-31 16:24:39 +0300

    add smile to page not found

--- src/components/page-not-found/PageNotFound.jsx
+++ src/components/page-not-found/PageNotFound.jsx
@@ -3,8 +3,9 @@ import "./styles.css";
 export default function PageNotFound() {
   return (
     <div className="pageNotFound">
+      {/* https://www.w3schools.com/charsets/ref_emoji_smileys.asp */}
       <p className="pageNotFound__text">
-        Oops, something went wrong. 404 error
+        Oops, something went wrong. 404 error &#128533;
       </p>
     </div>
   );
