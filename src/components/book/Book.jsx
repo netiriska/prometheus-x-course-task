35dc362e1d1d25c8f6df22fea973f359f40cfd7d
netiriska <i.sidik@ukr.net>
2023-08-20 17:55:12 +0300

    destructure props for Book

--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -1,11 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import imageNotFound from "../../images/specific-book/imageNotFoundVertical.png";
 
-export default function Book(props) {
+export default function Book({ setCurrentBook, book }) {
   const navigate = useNavigate();
+  const { image, title, author, price } = book;
 
   const navigateBook = () => {
-    props.setCurrentBook(props.book);
+    setCurrentBook(book);
     navigate("/specificbook");
   };
 
@@ -18,23 +19,21 @@ export default function Book(props) {
     <article className="book">
       <img
         className="book__image"
-        src={props.book.image}
+        src={image}
         alt="Book"
         onError={onErrorHandler}
       />
       <div className="book_short-info">
         <p className="book__title">
           <strong>Book name: </strong>
-          {props.book.title.length > 24
-            ? props.book.title.substring(0, 24) + "..."
-            : props.book.title}
+          {title.length > 24 ? title.substring(0, 24) + "..." : title}
         </p>
         <p className="book__author">
           <strong>Book author: </strong>
-          {props.book.author}
+          {author}
         </p>
         <div className="book__price_view">
-          <p className="book__price">${props.book.price}</p>
+          <p className="book__price">${price}</p>
           <button className="book__button" onClick={navigateBook}>
             View
           </button>
